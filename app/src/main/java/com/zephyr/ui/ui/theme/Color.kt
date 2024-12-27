206ed1a0a30e8f8c9b7c70d3122dbb2019c8406f
zheniaregbl <evgenieregbl@gmail.com>
2024-12-28 00:11:47 +0300

    init commit

---
+++ app/src/main/java/com/zephyr/ui/ui/theme/Color.kt
@@ -0,0 +1,6 @@
+package com.zephyr.ui.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val PrimaryColor = Color(0xFF57B9FF)
+val TertiaryOne = Color(0xFF77B1D4)
---
+++ app/src/main/java/com/zephyr/ui/ui/theme/Theme.kt
@@ -0,0 +1,17 @@
+package com.zephyr.ui.ui.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ZephyrTheme(
+    content: @Composable () -> Unit
+) {
+
+    MaterialTheme(
+        colorScheme = lightColorScheme(),
+        typography = Typography,
+        content = content
+    )
+}
