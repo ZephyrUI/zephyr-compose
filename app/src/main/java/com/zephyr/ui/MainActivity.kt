980bfa0105078e39ac0536da36789e4bab64c8b2
zheniaregbl <evgenieregbl@gmail.com>
2024-12-31 14:56:29 +0300

    refactor: delete unused imports

--- app/src/main/java/com/zephyr/ui/MainActivity.kt
+++ app/src/main/java/com/zephyr/ui/MainActivity.kt
@@ -4,16 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.zephyr.foundations.core.theme.ZephyrTheme
-import com.zephyr.foundations.element.button.AnimateButton
-import com.zephyr.sample.SimplyButton
 import com.zephyr.sample.SimplyOutlineButton
 
 class MainActivity : ComponentActivity() {
