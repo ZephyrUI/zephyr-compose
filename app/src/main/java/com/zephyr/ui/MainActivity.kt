7278a7ff7150f6b84240e820fb269b89af6d9f14
zheniaregbl <evgenieregbl@gmail.com>
2025-01-15 23:33:23 +0300

    feat: add AnimatedRadioButton in foundation module

--- app/src/main/java/com/zephyr/ui/MainActivity.kt
+++ app/src/main/java/com/zephyr/ui/MainActivity.kt
@@ -5,7 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.zephyr.foundations.core.theme.ZephyrTheme
-import com.zephyr.sample.SimpleCheckbox
+import com.zephyr.sample.SimpleRadioButton
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,7 +17,7 @@ class MainActivity : ComponentActivity() {
 
             ZephyrTheme {
 
-                SimpleCheckbox()
+                SimpleRadioButton()
             }
         }
     }
